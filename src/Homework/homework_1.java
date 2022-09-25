36cd4a0ed6b6f6e69821fd88221b6655c5013ae0
w10 <glsmh.16@gmail.com>
2022-09-25 21:25:33 +0300

    h

---
+++ src/Homework/homework_1.java
@@ -0,0 +1,35 @@
+package Homework;
+
+public class homework_1 {
+    public static void main(String[] args) {
+        String OS="Mac";
+
+
+        if(OS=="Mac"){
+            System.out.println("MacOS");
+        }else if (OS=="Lenova") {
+            System.out.println("Windows");
+        } else if (OS=="Iphone") {
+            System.out.println("IOS");
+        } else if (OS=="Samsung") {
+            System.out.println("Android");
+        }
+
+        }
+
+    }
+
+
+
+
+
+    /*    Write a java program in order to learn to OS
+        Mac     ->     MacOS
+        Lenova  ->     Windows
+        Iphone  ->     IOS
+        Samsung ->     Android
+
+        input : Mac
+        output: MacOS
+
+     */
