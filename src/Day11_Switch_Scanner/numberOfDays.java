680b3277d4ab6395f038c514af8f3947d7505cf6
w10 <glsmh.16@gmail.com>
2022-09-29 10:19:53 +0300

    h

--- src/Day11_Switch_Scanner/numberOfDays.java
+++ src/Day11_Switch_Scanner/numberOfDays.java
@@ -4,11 +4,11 @@ public class numberOfDays {
     public static void main(String[] args) {
         int year=2000;
         int number=9;
-         String result="";
+         String result = "";
         if (number>=1 && number<=12){
             switch (number) {
                 case 2:
-                    result =(number % 4 ==0) "29 days" : "28 days";
+                    result =(number % 4 == 0)  ?"29 days" : "28 days";
                     break;
                 case 4: case 6: case 9: case 11:
                     result = "30 days";
