911c63188a93add22641b42b5eb9abd21c4646ed
w10 <glsmh.16@gmail.com>
2022-10-04 10:23:08 +0300

    h

--- src/Day14_String/StringMethods2.java
+++ src/Day14_String/StringMethods2.java
@@ -17,5 +17,32 @@ public class StringMethods2 {
 
         System.out.println("sentence2 = " + sentence2);
 
+        String s="Dog Dog Dog Dog Dog Dog";
+        s=s.replace("Dog", "Cat");
+        System.out.println("s = " + s);
+
+        String s2= "C# is fun, C# is cool";
+        s2=s2.replace(" C#", " Java");
+        System.out.println("s2 = " + s2);
+
+        String s3="Java";
+        s3=s3.replace("a", "e");
+        System.out.println("s3 = " + s3);
+
+        System.out.println("----------------------------");
+
+        String result="Java Java Java";
+       // result=result.replace("Java", "Phyton");
+        result=result.replaceFirst("Java", "Phyton");
+        System.out.println("result = " + result);
+
+        String result2="C# is fun, C# is cool";
+        result2=result2.replaceFirst("C#", "Java");
+        System.out.println(result2);
+
+        String result3="Java";
+        result3=result3.replaceFirst("a", "o");
+        //result3=result3.replaceFirst("va", "vo");
+        System.out.println(result3);
     }
 }
