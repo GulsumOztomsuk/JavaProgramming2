2bb47f15cee413fd7d8695f698bc6f8c71435d50
w10 <glsmh.16@gmail.com>
2022-10-08 14:37:59 +0300

    h

---
+++ src/Homework/homework_2.java
@@ -0,0 +1,7 @@
+package Homework;
+
+public class homework_2 {
+    public static void main(String[] args) {
+        System.out.println("   J    a   v     v  a \n   J   a a   v   v  a a \nJ  J  aaaaa   v v  aaaaa\n JJ  a     a   v  a     a");
+    }
+}
