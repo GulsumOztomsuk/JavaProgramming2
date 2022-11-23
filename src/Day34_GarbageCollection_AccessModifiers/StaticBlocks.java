7962682ade286d7cae6a83eb7c2414d4269ca967
w10 <glsmh.16@gmail.com>
2022-11-23 20:21:42 +0300

    h

---
+++ src/Day34_GarbageCollection_AccessModifiers/StaticBlocks.java
@@ -0,0 +1,22 @@
+package Day34_GarbageCollection_AccessModifiers;
+
+public class StaticBlocks {
+    public static void main(String[] args) {
+
+        System.out.println("Main Method");
+
+    }
+
+    static{ //runs first before anything, and only runs one time
+        System.out.println("Static Block 1");
+    }
+
+    static{
+        System.out.println("Static Block 2");
+    }
+
+    static{
+        System.out.println("Static Block 3");
+    }
+
+}
