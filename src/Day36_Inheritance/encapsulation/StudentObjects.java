d1035c11af4e8a5e55a1acd8fabacd10b34425aa
w10 <glsmh.16@gmail.com>
2022-11-28 23:19:30 +0300

    day36

---
+++ src/Day36_Inheritance/encapsulation/StudentObjects.java
@@ -0,0 +1,8 @@
+package Day36_Inheritance.encapsulation;
+
+public class StudentObjects {
+    public static void main(String[] args) {
+        Student student1=new Student("Danial",29,'M','D',"MIT");
+        System.out.println(student1);
+    }
+}
