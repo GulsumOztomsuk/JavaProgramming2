ced9ee8e4cca09f8b9965d35fde80482dca2a1d3
w10 <glsmh.16@gmail.com>
2022-12-14 00:06:34 +0300

    day38_Inheritance Continue-Method Overriding

--- src/Day38_Inheritance/carTask/BMW.java
+++ src/Day38_Inheritance/carTask/BMW.java
@@ -1,7 +1,7 @@
 package Day38_Inheritance.carTask;
 
 public class BMW extends Car{
-    public BMW(String brand, String model, int year, double price, String color, int miles) {
+    public BMW(String model, int year, double price, String color, int miles) {
         super("BMW", model, year, price, color, miles);
     }
     public void breaksDown(){
